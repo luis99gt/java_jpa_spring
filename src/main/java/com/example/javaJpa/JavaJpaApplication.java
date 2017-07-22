9b80d619047db406c3917077b916aee3cb5b5cb6
Luis Feliciano <luis99.gt@gmail.com>
2017-07-22 09:36:06 -0600

    Alojado a Heroku

---
+++ src/main/java/com/example/javaJpa/JavaJpaApplication.java
@@ -0,0 +1,12 @@
+package com.example.javaJpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaJpaApplication.class, args);
+	}
+}
